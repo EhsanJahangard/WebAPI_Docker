da9c67d7d7a379ce8c8383c03b0e3931d53be4b6
Ehsan Jahangard <ehsan.jahangard.2014@gmail.com>
2024-01-17 14:35:02 +0330

    approve docker container

--- WebApplication_Docker/Controllers/WeatherForecastController.cs
+++ WebApplication_Docker/Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace WebApplication_Docker.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -13,13 +14,50 @@ namespace WebApplication_Docker.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
+
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+            _logger.LogInformation("ehsan create api");
         }
+        //[HttpGet("GetTruck2")]
+        //// public List<TruckViewModel> GetTruck()
+        //public TruckViewModel GetTruck2()
+        //{
+        //    var str = new List<string>();
 
+
+        //    for (int i = 0; i < 10000; i++)
+        //    {
+        //        str.Add(i.ToString());
+        //    } 
+
+        //}
+        //[HttpGet]
+        //// public List<TruckViewModel> GetTruck()
+        //public TruckViewModel GetTruck()
+        //{
+        //    var lst = new List<TruckViewModel> {
+
+        //    new TruckViewModel{ Id=2,Age=20,Family="ehsan" },
+        //    new TruckViewModel{ Id=111,Age=30,Family="ehsan" },
+        //    new TruckViewModel{ Id=221,Age=40,Family="ehsan" },
+        //    new TruckViewModel{ Id=331,Age=20,Family="ehsan" },
+        //    };
+
+        //    var x = lst.Where(f => f.Id == 1).Select(f => new { ehsan = f.Id, aaa = f.Age });
+
+
+
+
+        //    lst.TrimExcess();
+
+
+        //    return new TruckViewModel { Id = 1, Age = 20, Family = "ehsan" };
+
+        //}
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<List<WeatherForecast>> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -27,7 +65,7 @@ namespace WebApplication_Docker.Controllers
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
-            .ToArray();
+            .ToList();
         }
     }
 }
---
+++ WebApplication_Docker/TruckViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication_Docker
+{
+    public class TruckViewModel
+    {
+        public int Id { get;  set; }
+        public int Age { get;  set; }
+        public string Family { get; set; }
+
+    }
+}
